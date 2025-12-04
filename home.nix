87ed5ac739798c22e18f53bfac13cd05cdb43cc5
stanfish06 <zyyu@umich.edu>
2025-12-04 01:32:37 -0500

    fix neovim config

--- home.nix
+++ home.nix
@@ -2,11 +2,10 @@
 
 {
   home.stateVersion = "24.05";
-  home.file.".config/nvim".source = pkgs.fetchFromGitHub {
-    owner = "stanfish06";
-    repo = "nvim";
-    rev = "master";
-    # sha256 = pkgs.lib.fakeSha256;
-    sha256 = "sha256-dnS1hEq4Hw873BWVXDIF2rwtE9gP2pBYAY9UPdAnjEw=";
+  xdg.configFile = {
+	  "nvim" = {
+		  source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/dots/nvim";
+		  recursive = true;
+	  };
   };
 }
