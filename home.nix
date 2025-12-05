ddf1ad531ec744f34f35586e8181fea08c7ffa3d
stanfish06 <zyyu@umich.edu>
2025-12-05 08:56:20 -0500

    update

--- home.nix
+++ home.nix
@@ -3,9 +3,15 @@
 {
   home.stateVersion = "24.05";
   xdg.configFile = {
-	  "nvim" = {
-		  source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/dots/nvim";
-		  recursive = true;
-	  };
+    "nvim" = {
+  	  source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/dots/nvim";
+  	  recursive = true;
+    };
   };
+  home.packages = with pkgs; [
+    fzf
+    ripgrep
+    rofi
+    dwl
+  ];
 }
