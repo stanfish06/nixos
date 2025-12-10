be346575267e41c4614373aa5f5ff52adfbfef75
stanfish06 <zyyu@umich.edu>
2025-12-10 07:17:05 -0500

    update

--- configuration-linux.nix
+++ configuration-linux.nix
@@ -20,6 +20,19 @@
     maple-mono.NF
     iosevka
   ];
+  # steam
+  nixpkgs.config.allowUnfree = true;
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    gamescopeSession.enable = true;
+  };
+  # currently not working well: have weird pixels
+  programs.gamescope = {
+    enable = true;
+    capSysNice = true;
+  };
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
