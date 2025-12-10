f679c1886911f0b8523fcfb2ea00b1900c1a68a1
stanfish06 <zyyu@umich.edu>
2025-12-09 23:05:35 -0500

    update: start quickshell with dwl

--- home.nix
+++ home.nix
@@ -7,9 +7,59 @@
 
 {
   home.stateVersion = "24.05";
+
   home.file = {
     ".tmux.conf" = {
       source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/dots/my-configs/tmux/linux/.tmux.conf";
+    };
+    ".local/bin/start-dwl" = {
+      text = ''
+        #!/usr/bin/env bash
+
+        dwl &
+        # wait until socket is ready, then start services
+        unset WAYLAND_DISPLAY
+        while [ -z "$WAYLAND_DISPLAY" ]; do 
+            sleep 0.1
+            export WAYLAND_DISPLAY=wayland-0
+            if [ -S "$XDG_RUNTIME_DIR/$WAYLAND_DISPLAY" ]; then
+                echo "socket found"
+                break
+            fi
+            unset WAYLAND_DISPLAY
+        done
+
+        systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
+        dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
+        systemctl --user start wayland-session.target
+      '';
+      executable = true;
+    };
+  };
+  systemd.user.targets.wayland-session = {
+    Unit = {
+      Description = "Wayland compositor session";
+      Documentation = [ "man:systemd.special(7)" ];
+      BindsTo = [ "graphical-session.target" ];
+      Wants = [ "graphical-session-pre.target" ];
+      After = [ "graphical-session-pre.target" ];
+    };
+  };
+  systemd.user.services.quickshell = {
+    Unit = {
+      Description = "QuickShell";
+      PartOf = [ "graphical-session.target" ];
+      After = [ "graphical-session.target" ];
+    };
+    Service = {
+      ExecStart = "${pkgs.new.quickshell}/bin/quickshell";
+      Restart = "on-failure";
+      Environment = [
+        "PATH=${config.home.profileDirectory}/bin:/run/current-system/sw/bin"
+      ];
+    };
+    Install = {
+      WantedBy = [ "wayland-session.target" ];
     };
   };
   xdg.configFile = {
@@ -48,5 +98,4 @@
     lazygit
     new.quickshell
   ];
-  # TODO: autostart quickshell with dwl
 }
