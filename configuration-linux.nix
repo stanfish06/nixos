323efb2e07bb0c38efbd5313fa4294d396f29424
Stan <zyyu@umich.edu>
2026-06-18 21:25:24 -0400

    udev

--- configuration-linux.nix
+++ configuration-linux.nix
@@ -133,6 +133,11 @@
     pulse.enable = true;
   };
 
+  # Vial keyboard access over hidraw (https://get.vial.today/manual/linux-udev.html)
+  services.udev.extraRules = ''
+    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{serial}=="*vial:f64c2b3c*", MODE="0660", GROUP="users", TAG+="uaccess", TAG+="udev-acl"
+  '';
+
   programs.zsh.enable = true;
 
   programs.firefox.enable = true;
