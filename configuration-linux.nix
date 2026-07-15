796bde823954bbff7fb454f211c02f65fb20777b
Stan <zyyu@umich.edu>
2026-07-15 10:26:18 -0400

    fix: keep GDM login screen awake

--- configuration-linux.nix
+++ configuration-linux.nix
@@ -138,7 +138,10 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
   # Enable the GNOME Desktop Environment.
-  services.displayManager.gdm.enable = true;
+  services.displayManager.gdm = {
+    enable = true;
+    autoSuspend = false;
+  };
   services.desktopManager.gnome.enable = true;
   # Configure keymap in X11
   services.xserver.xkb = {
